c8348bd93129f1beefd08bb12583eb74ea43f20b
Anthony Delgado <me@anthonydelgado.me>
2016-09-10 23:50:24 -0400

    add dark cards

--- src/Card.js
+++ src/Card.js
@@ -8,14 +8,15 @@ var Card = React.createClass({
 
     render: function () {
         return (
-            <div className="col s12 m6">
-                <div className="card horizontal">
+            <div className="col s12 m6 l4">
+                <div className="card horizontal grey darken-3">
                     <div className="card-image">
                         <img src={ this.props.poster } alt="image" />
                     </div>
                     <div className="card-stacked">
-                        <div className="card-content">
-                            <p>{ this.props.title }</p>
+                        <div className="card-content white-text">
+                            <span className="card-title">{ this.props.title }</span>
+                            <p></p>
                         </div>
                         <div className="card-action">
                             <a href={ this.props.url }>Learn More</a>
